f474e2c03c9fdb8712f9dd67d10590dbb2292a2f
FrontEndFlow <75460272+FrontEndFlow@users.noreply.github.com>
2021-01-04 11:19:04 +0530

    Add files via upload

---
+++ PRELOADER/main.js
@@ -0,0 +1,10 @@
+//  Made by GeeksLord.com 
+
+const loaderContainer = document.querySelector('.loader-container');
+
+window.addEventListener('load',()=>{
+    loaderContainer.classList.add('hide');
+})
+
+//  Made by GeeksLord.com 
+//  Made by GeeksLord.com 
